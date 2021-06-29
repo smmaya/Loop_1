a78ed6fa22006b9ffbbef6280d0b83d4e08ad965
Sławomir Majchrzak <52676366+smmaya@users.noreply.github.com>
2021-06-29 14:12:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pętle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100, suma = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                suma = suma + i;
+            }
+            Console.WriteLine("Suma " + n + " pierwszych liczb = " + suma);
+
+            Console.ReadKey();
+
+        }
+    }
+}
